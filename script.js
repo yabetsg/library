02f72baa9c150e31476592e761f266a8cb505084
yabets <yabets.g2@gmail.com>
2023-03-01 20:39:46 -0500

    Add styling, form, and functioning buttons

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const display = document.querySelector("#display");
+const display = document.querySelector(".display-parent");
 const title = document.querySelector(".title");
 const author = document.querySelector(".author");
 const page = document.querySelector(".page");
 const read = document.querySelector(".read");
 const form = document.querySelector(".form");
+// const readButton = document.querySelector(.)
 
 let myLibrary = [];
 
@@ -32,21 +33,55 @@ function showBooks(){
        bpage =books.page ;
         bread   =books.read ;
     }); 
-    console.log()
-      return btitle + " by "+ bauthor + " , " + bpage + " pages, "+ bread;
+      return  btitle + "\n"+ "\n"+bauthor + "\n"+"\n" + bpage + "\n"+ "\n"+ "\n";
 }
 
-
+ let count = 0;
 form.addEventListener("submit", (e)=>{
+   count++;
+   
     
     addBookToLibrary(title.value,author.value,page.value,read.value);
-    console.log(showBooks());
-    display.innerText = showBooks();
-    title.value = ""
-    author.value = ""
-    page.value = ""
-    read.value = ""
-    console.log(myLibrary);
+    var article = document.createElement('article');
+    let buttondiv = document.createElement('div');
+    var div = document.createElement('div');
+    
+    var removeButton = document.createElement('button');
+    let checkedButton = document.createElement('button');
+    buttondiv.appendChild(removeButton);
+    buttondiv.appendChild(checkedButton);
+    div.classList.add(`display-${count}`);
+    removeButton.classList.add(`display-${count}`);
+    article.appendChild(div);
+    
+    if(read.checked){
+        checkedButton.textContent = "Read";
+        checkedButton.style.backgroundColor = "green";
+        article.appendChild(checkedButton);
+    }else{
+        checkedButton.textContent = "Not Read";
+        checkedButton.style.backgroundColor = "red";
+        article.appendChild(checkedButton);        
+    }
+    article.appendChild(removeButton);
+    display.appendChild(article);  
+    console.log(display);  
+    const displaygrid = document.querySelector(`.display-${count}`);
+    
+    displaygrid.innerText = showBooks();
+    title.value = "";
+    author.value = "";
+    page.value = "";
+    read.value = "";
+    const buttongrid = document.querySelector(`button.display-${count}`);
+    buttongrid.textContent = "Remove";
+    console.log(buttongrid);
+    
+    buttongrid.addEventListener('click', e =>{
+        display.removeChild(article);
+        count++
+    });
+       
 });
 
 
